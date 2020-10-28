c028f44f7df528a0dc9df552bfc5a971a2f11cd6
Zayd <zsimjee@github.com>
2020-10-28 15:14:46 -0700

    Python setup files, install and enable aws-jupyter-proxy on install

---
+++ aws_glue_databrew_jupyter_extension/__init__.py
@@ -0,0 +1,6 @@
+def _jupyter_server_extension_paths():
+    return [{"module": "aws_jupyter_proxy"}]
+
+
+def _jupyter_nbextension_paths():
+    return []
---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name='aws_glue_databrew_jupyter_extension',
+    version='0.1.0',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'aws-jupyter-proxy',
+        'notebook'
+    ],
+    include_package_data=True,
+    data_files=[
+        (
+            "etc/jupyter/jupyter_notebook_config.d",
+            ["aws_glue_databrew_jupyter_extension/etc/aws_glue_databrew_jupyter_extension.json"],
+        )
+    ],
+)
