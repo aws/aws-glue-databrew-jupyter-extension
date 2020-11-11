2c9e65c06f8d99e6ff95f58f3e236c5a006c335d
Zayd <zaydsimjee@gmail.com>
2020-11-10 17:50:01 -0800

    v1, icons, enhanced loading

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='aws_glue_databrew_jupyter_extension',
-    version='0.1.0',
+    version='1.0.0',
     packages=setuptools.find_packages(),
     install_requires=[
         'aws-jupyter-proxy',
--- src/aws_glue_databrew_extension.ts
+++ src/aws_glue_databrew_extension.ts
@@ -1,10 +1,14 @@
 import {
-  JupyterFrontEnd, JupyterFrontEndPlugin, ILayoutRestorer,
+  JupyterFrontEnd, JupyterFrontEndPlugin, ILayoutRestorer
 } from '@jupyterlab/application';
 
 import {
   ICommandPalette,
 } from '@jupyterlab/apputils';
+
+import {
+  ILauncher
+} from '@jupyterlab/launcher';
 
 import {
   Widget,
@@ -13,6 +17,12 @@ import {
 import {
   CommandRegistry,
 } from '@lumino/commands';
+
+import {
+  LabIcon
+} from '@jupyterlab/ui-components';
+
+import DATABREW_ICON from '../style/icons/databrew.svg';
 
 type GetPathsFunction = () => Promise<string[]>;
 const GLUE_DATABREW_RENDER = 'gluedatabrew:render';
@@ -76,6 +86,7 @@ export class GlueDataBrewConsoleWidget extends Widget {
     this.title.closable = true;
 
     this.consoleRoot = document.createElement('html');
+    this.consoleRoot.setAttribute('style', 'height: 100%');
     this.node.appendChild(this.consoleRoot);
 
     this.consoleRoot.insertAdjacentHTML('beforeend',
@@ -88,11 +99,13 @@ export class GlueDataBrewConsoleWidget extends Widget {
           <link rel="stylesheet" href=` + cssPath + `>
           <style>
             .loader {
-              border: 16px solid #f3f3f3;
+              display: inline-block;
+              border: 5px solid #f3f3f3;
               border-radius: 50%;
-              border-top: 16px solid #3498db;
-              width: 120px;
-              height: 120px;
+              border-top: 5px solid #3498db;
+              width: 40px;
+              height: 40px;
+              margin-right: 12px;
               -webkit-animation: spin 2s linear infinite;
               animation: spin 2s linear infinite;
             }
@@ -108,15 +121,23 @@ export class GlueDataBrewConsoleWidget extends Widget {
             }
 
             .loader-container {
-              margin: 12px;
+              font-size: 60px;
+              font-weight: 100;
+              color: #879196;
+              height: 100%;
+              width: 100%;
+              justify-content: center;
+              display: flex;
+              align-items: center;
             }
           </style>
         </head>
-        <body>
-          <div id="app">
-            <div class="loader-container>
-              <div style="font-size: 16px; margin-bottom: 12px;">Loading</div>
-              <div class="loader"/>
+        <body style="height: 100%">
+          <div id="app" style="height: 100%">
+            <div class="loader-container">
+              <div>
+                <div class="loader"></div><span>Launching AWS Glue DataBrew</span>
+              </div>
             </div>
           </div>
         </body>
@@ -129,14 +150,20 @@ export class GlueDataBrewConsoleWidget extends Widget {
  * Initialize the console widget extension
  */
 export const initiateExtension = (getPaths: GetPathsFunction) => {
-  const activate = async (app: JupyterFrontEnd, palette: ICommandPalette, restorer: ILayoutRestorer) => {
+  const activate = async (app: JupyterFrontEnd, palette: ICommandPalette, restorer: ILayoutRestorer, launcher: ILauncher) => {
 
     // Declare a widget variable
     const [jsPath, cssPath] = await getPaths();
     const consoleWidget: GlueDataBrewConsoleWidget = new GlueDataBrewConsoleWidget(cssPath);
 
+    const databrewIcon = new LabIcon({
+      name: 'gluedatabrew:icon',
+      svgstr: DATABREW_ICON
+    });
+
     app.commands.addCommand(GLUE_DATABREW_RENDER, {
       label: 'Launch AWS Glue DataBrew',
+      icon: databrewIcon,
       execute: () => {
         if (!consoleWidget.isAttached) {
           app.shell.add(consoleWidget, 'main');
@@ -151,7 +178,7 @@ export const initiateExtension = (getPaths: GetPathsFunction) => {
 
     const launcherWidget = new LeftSideLauncherWidget(app.commands);
     launcherWidget.id = 'aws_glue_databrew_jupyter_left_side_launcher';
-    launcherWidget.title.iconClass = 'jp-SpreadsheetIcon jp-SideBar-tabIcon';
+    launcherWidget.title.iconClass = 'jp-databrew-logo jp-SideBar-tabIcon';
     launcherWidget.title.caption = 'AWS Glue DataBrew';
 
     restorer.add(launcherWidget, launcherWidget.id);
@@ -159,12 +186,25 @@ export const initiateExtension = (getPaths: GetPathsFunction) => {
 
     // Add the command to the palette.
     palette.addItem({ command: GLUE_DATABREW_RENDER, category: 'Launcher' });
+    if (launcher) {
+      const launcher_item : ILauncher.IItemOptions = {
+          command: GLUE_DATABREW_RENDER,
+          args: {
+            newBrowserTab: true,
+            title: 'Launch Databrew',
+            id: 'databrew-launcher'
+          },
+          category: 'Other'
+      };
+      
+      launcher.add(launcher_item);
+    }
   };
 
   const extension: JupyterFrontEndPlugin<void> = {
     id: 'aws_glue_databrew_jupyter',
     autoStart: true,
-    requires: [ICommandPalette, ILayoutRestorer],
+    requires: [ICommandPalette, ILayoutRestorer, ILauncher],
     activate: activate,
   };
 
