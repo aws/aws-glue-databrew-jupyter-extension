be36add4375c671395e8de537058d1b63bd7fa86
Kevin Pagtakhan <pagtkevi@amazon.com>
2021-03-12 12:25:57 -0800

    Remove generated files and add prepublishOnly script

--- lib/client.d.ts
+++
@@ -1,6 +0,0 @@
-import { AWSConfig } from "./types";
-export declare const getAWSConfig: (basePath: string) => Promise<AWSConfig>;
-export declare const get: <T>(path: string) => Promise<T>;
-export declare const post: <T>(path: string) => Promise<T>;
-export declare const request: <T>(req: Request) => Promise<T>;
-export declare const parse: <T>(text: string) => T;
--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const getPaths: () => Promise<string[]>;
-export declare const getPrefix: () => Promise<string>;
-declare const extension: import("@jupyterlab/application").JupyterFrontEndPlugin<void>;
-export default extension;
