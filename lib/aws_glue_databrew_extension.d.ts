01875c00b8be36e3e5023dd323af8007360634f8
Kevin Pagtakhan <pagtkevi@amazon.com>
2021-03-12 12:25:57 -0800

    Remove generated files and add prepublishOnly script

--- lib/aws_glue_databrew_extension.d.ts
+++
@@ -1,5 +0,0 @@
-import { JupyterFrontEndPlugin } from "@jupyterlab/application";
-/**
- * Initialize the console widget extension
- */
-export declare const initiateExtension: (getPaths: () => Promise<string[]>) => JupyterFrontEndPlugin<void>;
--- lib/types.d.ts
+++
@@ -1,4 +0,0 @@
-export declare type AWSRegion = string | undefined | null;
-export interface AWSConfig {
-    region: AWSRegion;
-}
